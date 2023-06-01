2ec803b6b10fd294681f9fea4f4c9cac7f26ce63
mesutcode <mesutcode@gmail.com>
2023-06-01 23:34:22 +0300

    Add tamstack-react-query-devtools -Remove useReducer -Add useGetProductQuery hooks

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,72 +1,24 @@
 import { Col, Row } from 'react-bootstrap'
-import { useEffect, useReducer } from 'react'
-import axios from 'axios'
-import { Product } from './types/Product'
 import LoadingBox from './LoadingBox'
 import MessageBox from './MessageBox'
 import { ApiError, getError } from './utils'
 import ProductCard from './ProductCard'
 import { Helmet } from 'react-helmet-async'
-
-type State = {
-  products: Product[]
-  loading: boolean
-  error: string
-}
-
-type Action =
-  | { type: 'FETCH_REQUEST' }
-  | { type: 'FETCH_SUCCESS'; payload: Product[] }
-  | { type: 'FETCH_FAIL'; payload: string }
-
-const initialState: State = {
-  products: [],
-  loading: true,
-  error: '',
-}
-
-const reducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true }
-    case 'FETCH_SUCCESS':
-      return { ...state, products: action.payload, loading: false }
-    case 'FETCH_FAIL':
-      return { ...state, error: action.payload, loading: false }
-    default:
-      return state
-  }
-}
+import { useGetProductsQuery } from './hooks/productHooks'
 
 export default function Home() {
-  const [{ products, error, loading }, dispatch] = useReducer(
-    reducer,
-    initialState
-  )
+  const { data: products, isLoading, error } = useGetProductsQuery()
 
-  useEffect(() => {
-    async function getProducts() {
-      dispatch({ type: 'FETCH_REQUEST' })
-      try {
-        const result = await axios.get('/api/products')
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
-      } catch (error) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(error as ApiError) })
-      }
-    }
-    getProducts()
-  }, [])
-
-  return loading ? (
+  return isLoading ? (
     <LoadingBox />
   ) : error ? (
-    <MessageBox variant={'danger'}>{error}</MessageBox>
+    <MessageBox variant={'danger'}>{getError(error as ApiError)}</MessageBox>
   ) : (
     <Row>
       <Helmet>
         <title>TS mesutnisebin</title>
       </Helmet>
-      {products.map((product) => (
+      {products!.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
           <ProductCard product={product} />
         </Col>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,35 +6,36 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
 import Home from './Home'
 import Product from './Product'
-import axios from 'axios'
 import { HelmetProvider } from 'react-helmet-async'
-
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<Home />} />
       <Route path="product/:slug" element={<Product />} />
-      {/* <Route path="dashboard" element={<Dashboard />} /> */}
-      {/* ... etc. */}
     </Route>
   )
 )
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        {/* The rest of your application */}
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 )
