633a09f49cf7ee79c31fa1bc5ddb225628a920a5
mesutcode <mesutcode@gmail.com>
2023-06-01 21:57:33 +0300

    Add Rating.tsx -Create ProductCard.tsx

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,11 +1,11 @@
 import { Col, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import { useEffect, useReducer } from 'react'
 import axios from 'axios'
 import { Product } from './types/Product'
 import LoadingBox from './LoadingBox'
 import MessageBox from './MessageBox'
 import { ApiError, getError } from './utils'
+import ProductCard from './ProductCard'
 
 type State = {
   products: Product[]
@@ -64,15 +64,7 @@ export default function Home() {
     <Row>
       {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={'/product/' + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductCard product={product} />
         </Col>
       ))}
     </Row>
